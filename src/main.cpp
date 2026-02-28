e08cf0a212fb6768afa96c258378c64c4183de7f
Steven_Lee <11872373+steven_lee_147@user.noreply.gitee.com>
2026-02-28 18:14:56 +0800

    refactor(main): 简化入口点，委托给 App 类

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,13 @@
-#include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include "core/app.h"
 
-int main(int argc, char* argv[])
+int main(int /*argc*/, char* /*argv*/[])
 {
-    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO))
+    sakura::core::App app;
+    if (app.Initialize())
     {
-        SDL_Log("SDL init failed: %s", SDL_GetError());
-        return 1;
+        app.Run();
     }
-
-    SDL_Window* window = SDL_CreateWindow("Sakura-樱", 1280, 720, SDL_WINDOW_RESIZABLE);
-    if (!window)
-    {
-        SDL_Log("Window creation failed: %s", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-    bool running = true;
-    while (running)
-    {
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_EVENT_QUIT)
-            {
-                running = false;
-            }
-        }
-    }
-
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    app.Shutdown();
     return 0;
 }
